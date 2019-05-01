a7824e8c0129d0459b2a1fa8fdd044b58caa0718
petern160 <peternguyen160@gmail.com>
2019-05-01 23:51:52 +0000

    finished oop

--- index.js
+++ index.js
@@ -2,29 +2,30 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+  
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
 }
 
-// prints out the provided task's details
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
-
-// marks the provided task as completed
-function completeTask(task) {
-  task.complete = true;
-}
-
-// DRIVER CODE BELOW
+// DRIVER CODE CHANGES BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
-
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
 console.log(tasks);
+
+test
+task1.title
+task1.markCompleted();
